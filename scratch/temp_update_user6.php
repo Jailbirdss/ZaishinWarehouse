e6fa779e80f0c1be99246d8db64b5820a2555d26
Jailbirdss <221957219+Jailbirdss@users.noreply.github.com>
2026-07-11 10:52:00 +0700

    Remove temp update user script

--- scratch/temp_update_user6.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/database.php';
-$db = getDB();
-$db->query('UPDATE users SET name = \'Fandy Ahmad\', username = \'fandy\', password = \'$2y$10$d6qUZGmAGRJeXp1W/kEZjeoezJEwR.iyBJkr0J4ykp7DDxmof14/e\', avatar = \'FA\' WHERE id = 6');
-echo "Updated successfully!\n";
